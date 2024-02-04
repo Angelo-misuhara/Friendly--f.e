e3b079097d538241faa7de4d2ac5129d8f18d703
ogel <yocorangelo18@gmail.com>
2024-02-04 21:32:36 +0800

    plantso

--- src/Pages/PersonMessage.jsx
+++ src/Pages/PersonMessage.jsx
@@ -4,12 +4,12 @@ import { Input } from '@chakra-ui/react'
 const PersonMessage = () => {
   return (
    <>
-    <div className=' flex flex-col mx-11 gap-12 md:mt-0 mt-3 '>
+    <div className=' flex flex-col mx-11 gap-12 md:mt-0 '>
      <div className='flex flex-row gap-2'>
       <img src="/img/online.png" alt="" />
       <h1 className='font-bold text-[1.3rem]'>Aya</h1>
      </div>
-     <div id='scrollPersonalMessage' className='flex flex-col gap-6 p-5 h-[38rem] bg-scroll overscroll-contain overflow-y-scroll'>
+     <div id='scrollPersonalMessage' className='flex flex-col gap-6 p-5 md:h-[38rem] h-[30rem] bg-scroll overscroll-contain overflow-y-scroll'>
        <div className='flex flex-row gap-2'>
         <img src="/img/ayamarkup.jpg" alt="" className='w-[2rem] h-[2rem] rounded-full' />
         <p className='border rounded-md p-2 '>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam natus ratione tempora dolorem consectetur. Praesentium, dolor! Animi libero quidem ut!</p>
@@ -62,7 +62,7 @@ const PersonMessage = () => {
         </div>
         
       </div>
-      <div className=''>
+      <div className=' f'>
         <form action="" className='flex flex-row gap-2 mx-9'>
            <Input variant='flushed' placeholder='Message here' />
             <img src="img/sendMess.png" alt="" />
