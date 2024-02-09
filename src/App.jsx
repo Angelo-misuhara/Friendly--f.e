ba69acfaf482e23496377f250dde305835538a38
ogel <yocorangelo18@gmail.com>
2024-02-09 09:46:09 +0800

    markup

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import AppRoutes from './Routes'
 import './App.css'
 import Fries from './components/Fries'
 import Sidebar from './components/Sidebar'
+import CreatePost from './components/CreatePost'
+import AuthPage from './Pages/AuthPage'
+import Profile from './Pages/Profile'
+import Content from './Pages/Content'
+import PersonMessage from './Pages/PersonMessage'
 
 function App() {
 
@@ -17,9 +22,20 @@ function App() {
             <Fries />
           </div>
           <div className="md:basis-4/5 mt-11 lg:mx-32 ">
-            <Router>
+            {/* <Router>
               <AppRoutes />
-            </Router>
+            </Router> */}
+           
+            <div className='mb-[12rem]'>
+                <Profile />
+            </div>
+            <div className='mb-[12rem]'>
+ <Content />
+            </div>
+           
+            <PersonMessage/>
+
+            
           </div>
         </div>
       </div>
